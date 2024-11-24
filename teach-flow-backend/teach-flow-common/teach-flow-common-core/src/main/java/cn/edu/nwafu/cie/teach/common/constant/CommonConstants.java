603acab6a56106450b1d55ef67789d1949c08115
Huang Zhengyang <2691386627@qq.com>
2024-11-25 01:35:15 +0800

    feat: 请求响应体实体类

--- teach-flow-backend/teach-flow-common/teach-flow-common-core/src/main/java/cn/edu/nwafu/cie/teach/common/constant/CommonConstants.java
+++ teach-flow-backend/teach-flow-common/teach-flow-common-core/src/main/java/cn/edu/nwafu/cie/teach/common/constant/CommonConstants.java
@@ -6,7 +6,16 @@ package cn.edu.nwafu.cie.teach.common.constant;
  * @author Huang Z.Y.
  * @create 2024-11-19 01:23
  */
-public interface SystemConstants {
+public interface CommonConstants {
     String REDIS_PREFIX = "teach-flow:";
+    /**
+     * 成功标记
+     */
+    Integer SUCCESS = 0;
+
+    /**
+     * 失败标记
+     */
+    Integer FAIL = 1;
 }
     
